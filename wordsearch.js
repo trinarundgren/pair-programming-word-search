36ad5783758e456cf7efaa45408e00dcc43a8ffc
Trina <trina.rundgren@telus.com>
2022-11-23 20:40:11 -0800

    final version

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,31 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
-    }
-}
 
-module.exports = wordSearch
+const wordSearch = (letters, word) => {
+  const horizontalJoin = letters.map(ls => ls.join(''));
+
+  for (let l of horizontalJoin) {
+    if (l.includes(word.toUpperCase())) return true;
+  }
+
+  let vertArr = [];
+  let vertArrSize = letters.length;
+  let horiArrSize = letters[0].length;
+  
+  //for each array inside of letters
+  for (let i = 0; i < horiArrSize - 1; i++) {
+    for (let j = 0; j < vertArrSize; j++) {
+      //pushes all of one index into arr.
+      vertArr.push(letters[j][i]);
+      if (vertArr.length === vertArrSize) {
+        let newStr = vertArr.join("");
+        if (newStr.includes(word.toUpperCase())) {
+          return true;
+        } else {
+          vertArr = [];
+        }
+      }
+    }
+  }
+  return false;
+};
+
+module.exports = wordSearch;
